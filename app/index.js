4e67ce82620c1de0a45bf0d8434649d3ddded619
Sylvain Rougié <46543145+Sipixer@users.noreply.github.com>
2025-09-06 10:25:21 +0200

    test2

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 80;
 
-app.get("/", (req, res) => res.send("Hello TESss GitOps!"));
+app.get("/", (req, res) => res.send("Hello TESssss GitOps!"));
 
 app.listen(port, () => console.log(`App running on port ${port}`));
