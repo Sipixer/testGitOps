782dfffb0c972ad51b783f633ab085ceabdbadd4
Sylvain Rougié <46543145+Sipixer@users.noreply.github.com>
2025-09-07 19:27:10 +0200

    feat test github actions

--- app/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 80;
-
-app.get("/", (req, res) => res.send("Hello GitOps!"));
-
-app.listen(port, () => console.log(`App running on port ${port}`));
--- k8s/base/helm-chart/templates/_helpers.tpl
+++
@@ -1,7 +0,0 @@
-{{- define "app-chart.name" -}}
-app
-{{- end -}}
-
-{{- define "app-chart.fullname" -}}
-{{ include "app-chart.name" . }}-{{ .Values.branch }}
-{{- end -}}
